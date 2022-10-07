9c16d968b127e3b34620fad205b7f5ac346db72c
Sou <33419855+soumindar@users.noreply.github.com>
2022-10-07 11:20:51 +0700

    init

---
+++ 10 days of JS/buttons container/buttons.js
@@ -0,0 +1,8 @@
+let divElement = document.createElement("Div");
+divElement.id = "btns";
+
+let btn1 = document.createElement("Button");
+btn1.innerHTML = "1";
+divElement.appendChild(btn1);
+
+document.body.appendChild(divElement);
